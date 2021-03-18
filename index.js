d75c6f8d9261a26e691a62c61e4d5f4d6c25fc9c
HacLin <muthuhari234@gmail.com>
2021-03-18 18:04:37 +0530

    update index.js

--- index.js
+++ index.js
@@ -61,14 +61,14 @@ bot.command('anime', (ctx) => {
                     //console.log(response);
                     // console.log(body);
                     var res = JSON.parse(body);
-                    Results.push([res]);
+                    Results.push(res);
                     // console.log(Results);
                     // console.log(Results[0]);
                     // console.log(Results[0].results[0].image_url);
                     // console.log(Results[0].res.results[0]);
                     console.log(Results[0].results);
                     // console.log(Results[0]['results'][0][image_url]);
-                    console.log(Results[0].results.image_url);
+                    // console.log(Results[0].results.image_url);
                     if (res.status == 404) {
                         ctx.reply(res.message + " Try Again Later!");
 
