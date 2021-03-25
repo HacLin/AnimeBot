3129e91cc799cccdcff04eb1942cef659a60454e
HacLin <muthuhari234@gmail.com>
2021-03-26 01:22:40 +0530

    globalised data sender function and bot fully functioning

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ DataRequest = (Item, page, type) => {
                     reject(res.message);
                 }
 
-                console.log(Results);
+                // console.log(Results);
                 // console.log("Not Returned");
             }
         })
@@ -94,7 +94,7 @@ KeyboardBuilder = (Item, type, pageno, opcount, start, stop) => {
     // let Type = type;
     // let Items = Item;
     console.log("Searching for " + Item + " as " + type + " in the Results");
-    reply_message = `Loaded Page : ${pageno}` + '\n' + `Loaded Options : ${(pageno*opcount)-(opcount-stop)}`;
+    reply_message = 'Searching for : ' + Item + '\n' + `Loaded Options : ${(pageno*opcount)-(opcount-stop)}`;
     for (let i = start; i < stop; i++) {
         choices[i] = new Object();
         choices[i].Title = Results[Results.findIndex(x => x.callbackdata == cbdata)].results.results[i].title;
@@ -129,7 +129,34 @@ KeyboardSender = (repmsg, keydata, ctx) => {
         .catch(err => console.log(err))
 
 }
+Datasender = (cbdata, media, ctx) => {
 
+    // console.log(cbdata);
+    // console.log(media);
+    // console.log(ctx);
+    let run;
+    anime = () => {
+        let imageurl = Results[cbdata[1]].results.results[cbdata[2]].image_url;
+        let Title = Results[cbdata[1]].results.results[cbdata[2]].title;
+        let type = Results[cbdata[1]].results.results[cbdata[2]].type;
+        let Airing = () => {
+            if (Results[cbdata[1]].results.results[cbdata[2]].airing) {
+                return ("Currently Airing")
+            } else {
+                return ("Finished Airing")
+            }
+        }
+        let Episodes = Results[cbdata[1]].results.results[cbdata[2]].episodes;
+        let Score = Results[cbdata[1]].results.results[cbdata[2]].score;
+        let Rating = Results[cbdata[1]].results.results[cbdata[2]].rated;
+        let url = Results[cbdata[1]].results.results[cbdata[2]].url;
+        let plot = Results[cbdata[1]].results.results[cbdata[2]].synopsis;
+        ctx.replyWithPhoto(imageurl, { caption: "\n\nTitle: " + Title + "\nType: " + type + "\nStatus: " + Airing() + "\nNo.of.Episodes: " + Episodes + "\nScore: " + Score + "\nRating: " + Rating + "\n" + plot + "\n\nFor more info visit: " + url })
+    }
+    if (media[0] == "anime")
+        run = anime
+    run();
+}
 
 
 
@@ -174,16 +201,28 @@ bot.command('anime', async(ctx) => {
                 console.log("Received Callback Query Data :" + cbquery);
                 var cbdata = cbquery.split("-");
                 cbdata = cbdata.map((x) => { return parseInt(x, 10) })
-                console.log(cbdata);
+                    // console.log(cbdata);
                 if (cbdata.length == 2) {
                     let media = Results[cbdata[1]].callbackdata.split('-')
-                    console.log(media);
+                        // console.log(media);
                     let options = Results[cbdata[1]].loaded;
-                    console.log("Loading More Options for " + Results[cbdata[1]].callbackdata)
-                    let keydata = KeyboardBuilder(media[1], media[0], cbdata[0], opcount, options, options + 5)
-                    KeyboardSender(reply_message, keydata, ctx);
-                }
+                    if (options > opcount) {
+                        console.log("Resource does not exist")
+                        ctx.reply("Resource does not exist")
+                    } else {
+                        console.log("Loading More Options for " + Results[cbdata[1]].callbackdata)
+                        let keydata = KeyboardBuilder(media[1], media[0], cbdata[0], opcount, options, options + 5)
+                        KeyboardSender(reply_message, keydata, ctx);
+                    }
+                } else {
+                    let media = Results[cbdata[1]].callbackdata.split('-')
+                        // console.log(media);
+                        // console.log(Results[cbdata[1]].results.results[cbdata[2]]);
+                        // ctx.replyWithPhoto(Results[cbdata[1]].results.results[cbdata[2]])
+                    Datasender(cbdata, media, ctx);
+                    console.log("Data sent for " + media[0] + "-" + media[1]);
 
+                }
 
 
 
