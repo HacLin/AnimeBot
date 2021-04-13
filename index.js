dbe73aff0dedea2002d667166bdf86e36142c6b6
HacLin <muthuhari234@gmail.com>
2021-04-13 18:57:49 +0530

    Deleted Heroku Dyno,App Available locally

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const { Telegraf } = require('telegraf');
 const BOT_TOKEN = process.env.BOT_TOKEN
-const PORT = process.env.PORT || 3000
-const URL = process.env.BOT_DOMAIN
 const bot = new Telegraf(BOT_TOKEN);
 const request = require('request');
-bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
-bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT)
-require('heroku-self-ping').default(URL, console.log());
+
 
 
 bot.start((ctx) => {
